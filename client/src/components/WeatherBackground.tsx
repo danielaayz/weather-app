5aa733842d052d05a96f780731499fc67f327372
Daniela Y. Zagal <149149177+danielaayz@users.noreply.github.com>
2024-09-26 10:58:12 +0200

    feat: Add WeatherBackground component to dynamically change background color based on weather conditions

---
+++ client/src/components/WeatherBackground.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { WeatherData } from "./Weather";
+
+interface WeatherBackgroundProps {
+   weatherData: WeatherData | null;
+   children: React.ReactNode;
+}
+
+const WeatherBackground: React.FC<WeatherBackgroundProps> = ({
+   weatherData,
+   children,
+}) => {
+   // Funktion för att bestämma bakgrundfärg baserat på väder
+   const getBackgroundColor = (): string => {
+      if (!weatherData || !weatherData.weather[0]) {
+         // Standard bakgrundsfärg
+         return "#ffffff";
+      }
+      // Hämtar väderbeskrivningen från det första elementen i weather-arrayen
+      const weatherMain = weatherData.weather[0].main;
+
+      // switch-sats för att returnera olika bakgrundsfärger beroende på vädret
+      switch (weatherMain.toLowerCase()) {
+         case "clear":
+            return "bg-[#FFF1C7]";
+         case "clouds":
+            return "bg-[#D9D9D9]";
+         case "rain":
+            return "bg-[#C2DFF0]";
+         case "snow":
+            return "bg-[#E0F7FA]";
+         case "thunderstorm":
+            return "bg-[#50597B]";
+         case "mist":
+         case "fog":
+            return "bg-[#E6E6E6]";
+         default:
+            return "#F0F8FF";
+      }
+   };
+
+   return (
+      <div
+         className={`${getBackgroundColor()} min-h-screen transition-colors duration-500 flex justify-center items-center`}>
+         {children}
+      </div>
+   );
+};
+
+export default WeatherBackground;
